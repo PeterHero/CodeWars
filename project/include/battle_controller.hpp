005e847da206469782403478fa2fbe634a70d97d
Petr Hrdina <phrdina@proton.me>
2025-02-19 07:45:48 +0100

    Add objects on battlefield

--- project/include/battle_controller.hpp
+++ project/include/battle_controller.hpp
@@ -2,33 +2,58 @@
 #define battle_controller_hpp_
 
 #include <array>
+#include <iostream>
 #include <map>
 
 #define FIELD_SIZE 5
 #define ROBOT_MAX_HEALTH 100
 #define ROBOT_NULL_ID 0
 
+using robot_id_t = size_t;
+
 enum class Direction { LEFT, UP, RIGHT, DOWN };
 
 class Robot {
 private:
 public:
-  size_t _id;
+  robot_id_t _id;
   size_t _health;
   size_t _pos_x;
   size_t _pos_y;
   Direction _look_dir;
 
   Robot();
-  Robot(size_t id, size_t pos_x, size_t pos_y, Direction direction);
+  Robot(robot_id_t id, size_t pos_x, size_t pos_y, Direction direction);
+  void print(std::ostream &stream = std::cout);
 };
+
+std::ostream &operator<<(std::ostream &stream, Robot &robot);
+
+class FieldObject {
+public:
+  enum class Type { GROUND, WALL };
+
+private:
+  Type _type;
+
+public:
+  FieldObject();
+  FieldObject(Type type);
+  bool is_walkable();
+  void print(std::ostream &stream = std::cout);
+};
+
+std::ostream &operator<<(std::ostream &stream, FieldObject &field_object);
 
 class BattleController {
 private:
-  std::array<std::array<size_t, FIELD_SIZE>, FIELD_SIZE> _robots_on_field;
-  std::map<size_t, Robot> _robots;
+  std::array<std::array<FieldObject, FIELD_SIZE>, FIELD_SIZE> _battlefield;
+  std::array<std::array<robot_id_t, FIELD_SIZE>, FIELD_SIZE> _robots_on_field;
+  std::map<robot_id_t, Robot> _robots;
 
-  void create_robot(size_t id, size_t pos_x, size_t pos_y, Direction direction);
+  void create_robot(robot_id_t id, size_t pos_x, size_t pos_y,
+                    Direction direction);
+  void move_robot(robot_id_t id, Direction direction); // todo
 
 public:
   BattleController();
--- project/src/battle_controller.cpp
+++ project/src/battle_controller.cpp
@@ -3,49 +3,91 @@
 
 Robot::Robot() {}
 
-Robot::Robot(size_t id, size_t pos_x, size_t pos_y, Direction direction)
+Robot::Robot(robot_id_t id, size_t pos_x, size_t pos_y, Direction direction)
     : _id(id), _health(ROBOT_MAX_HEALTH), _pos_x(pos_x), _pos_y(pos_y),
       _look_dir(direction) {}
 
-BattleController::BattleController() : _robots_on_field({}) {
+void Robot::print(std::ostream &stream) {
+  switch (_look_dir) {
+  case Direction::LEFT:
+    stream << "<";
+    break;
+  case Direction::UP:
+    stream << "A";
+    break;
+  case Direction::RIGHT:
+    stream << ">";
+    break;
+  case Direction::DOWN:
+    stream << "V";
+    break;
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream, Robot &robot) {
+  robot.print(stream);
+  return stream;
+}
+
+FieldObject::FieldObject() : _type(Type::GROUND) {}
+
+FieldObject::FieldObject(Type type) : _type(type) {}
+
+bool FieldObject::is_walkable() {
+  switch (_type) {
+  case Type::GROUND:
+    return true;
+  case Type::WALL:
+    return false;
+  default:
+    return false;
+  }
+}
+
+void FieldObject::print(std::ostream &stream) {
+  switch (_type) {
+  case Type::GROUND:
+    stream << " ";
+    break;
+  case Type::WALL:
+    stream << "W";
+    break;
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream, FieldObject &field_object) {
+  field_object.print(stream);
+  return stream;
+}
+
+BattleController::BattleController() : _robots_on_field({}), _battlefield({}) {
+  _battlefield[0][1] = FieldObject(FieldObject::Type::WALL);
+
   create_robot(1, 0, 0, Direction::RIGHT);
   create_robot(2, 1, 1, Direction::DOWN);
   create_robot(3, 3, 3, Direction::LEFT);
   create_robot(4, 4, 4, Direction::UP);
 }
 
-void BattleController::create_robot(size_t id, size_t pos_x, size_t pos_y,
+void BattleController::create_robot(robot_id_t id, size_t pos_x, size_t pos_y,
                                     Direction direction) {
   _robots[id] = Robot(id, pos_x, pos_y, direction);
   _robots_on_field[pos_x][pos_y] = id;
 }
 
 void BattleController::print_battlefield() {
-
   for (size_t i = 0; i < FIELD_SIZE + 2; ++i)
     std::cout << "X";
   std::cout << std::endl;
 
-  for (auto row : _robots_on_field) {
+  for (size_t i = 0; i < FIELD_SIZE; ++i) {
     std::cout << "X";
-    for (size_t id : row) {
-      if (id == ROBOT_NULL_ID) {
-        std::cout << " ";
+    for (size_t j = 0; j < FIELD_SIZE; ++j) {
+      robot_id_t robot_id = _robots_on_field[i][j];
+      if (robot_id == ROBOT_NULL_ID) {
+        std::cout << _battlefield[i][j];
       } else {
-        switch (_robots[id]._look_dir) {
-        case Direction::LEFT:
-          std::cout << "<";
-          break;
-        case Direction::UP:
-          std::cout << "A";
-          break;
-        case Direction::RIGHT:
-          std::cout << ">";
-          break;
-        case Direction::DOWN:
-          std::cout << "V";
-          break;
-        }
+        std::cout << _robots[robot_id];
       }
     }
     std::cout << "X" << std::endl;
